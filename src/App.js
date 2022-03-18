636c88eabf7746859887d0f57b7e8f8883be58c5
iinsue <hinsu93@naver.com>
2022-03-18 09:48:43 +0900

    14.4 | 뉴스 뷰어 UI 만들기(366p)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import {useState} from "react";
-import axios from "axios";
+import NewsList from "./components/NewsList";
 
 
 function App() {
-  const SECRET_API_KEY = process.env.REACT_APP_API_KEY;
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try{
-    const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${SECRET_API_KEY}`);
-    setData(response.data);
-    } catch(e){
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
-}
+  return <NewsList/>;
+};
 
 export default App;
